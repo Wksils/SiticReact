52d01618528c55be4d1fa472991f97619d1a7182
Wksils <y.ovchinnikova08@gmail.com>
2026-09-19 13:03:30 +0200

    Update App.jsx

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,4 +1,5 @@
 import Section1 from "./components/Section1/section1"
+import Section2 from "./components/Section2/Section2"
 import './App.scss'
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
    return (
     <>
       <Section1/>
+      <Section2/>
     </>
   )
 }
