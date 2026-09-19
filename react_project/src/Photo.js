845c588da92cdb49f0139f72855cf73e00f7c253
Wksils <y.ovchinnikova08@gmail.com>
2026-09-19 13:00:01 +0200

    Update Photo.js

--- react_project/src/Photo.js
+++ react_project/src/Photo.js
@@ -1,5 +1,19 @@
-import bgs1 from "./assets/bgs1"
+import bgs1 from "./assets/bgs1.png"
+import amadeus from "./assets/amadeus.png"
+import booking from "./assets/booking.png"
+import expedia from "./assets/expedia.png"
+import google from "./assets/google.png"
+import skyscanner from "./assets/skyscanner.png"
+import sova from "./assets/sova.png"
+import travelport from "./assets/travelport.png"
 
 export const Photo = {
-    bgs1:bgs1
+    bgs1:bgs1,
+    amadeus:amadeus,
+    booking:booking,
+    expedia:expedia,
+    google:google,
+    skyscanner:skyscanner,
+    sova:sova,
+    travelport:travelport
 }
