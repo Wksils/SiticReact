670413780b5e8680c20161ebc849850723d0abeb
wksils <y.ovchinnikova08@gmail.com>
2026-09-20 22:07:24 +0200

    finish sect3

--- react_project/src/components/Section3/Section3.jsx
+++ react_project/src/components/Section3/Section3.jsx
@@ -2,28 +2,28 @@ import './section3.scss'
 import {Photo} from "../../Photo.js"
 
 export default function Section3(){
+    return(
     <>
         <section className='section3'>
             <div className='sectionContainer'>
                 <div className='top'>
-                    <div>
-                        <p>"</p>
-                    </div>
-                    <h2>AWDAafe</h2>
+                    <p>“</p>
+                    <h2>At Travalyst, we’re bringing travellers, communities, and the travel industry to the table to change the way we see and share the world.</h2>
                 </div>
-                <p></p>
+                <p className='text'>By working with Travalyst and our industry partners, we aim to build tools and technology that enable travelers and businesses around the world to prioritize sustainability.</p>
                 <div className='ppls'>
-                    <div className='arrow'><img src="" alt="" /></div>
+                    <button>&larr;</button>
                     <div className='ppl'>
-                        <div><img src={Photo.face1} alt="" /> <p></p></div>
+                        <div className='fp'><img src={Photo.face1} alt="" /> <p>Prince Harry </p><p>The Duke of Sussex</p></div>
                         <div><img src={Photo.face2} alt="" /></div>
                         <div><img src={Photo.face3} alt="" /></div>
                         <div><img src={Photo.face4} alt="" /></div>
                         <div><img src={Photo.face5} alt="" /></div>
                     </div>
-                    <div className='arrow'><img src="" alt="" /></div>
+                    <button>&rarr;</button>
                 </div>
             </div>
         </section>
     </>
+    )
 }
