b46cd711ea38fa50558e1eb1b1152003fd6bde3b
YanaOw <y.ovchinnikova08@gmail.com>
2026-09-21 22:35:58 +0200

    nemnogo section5

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -2,6 +2,7 @@ import Section1 from "./components/Section1/section1"
 import Section2 from "./components/Section2/Section2"
 import Section3 from "./components/Section3/Section3"
 import Section4 from "./components/Section4/Section4"
+import Section5 from "./components/Section5/Section5"
 import './App.scss'
 
 export default function App() {
---
+++ react_project/src/components/Section5/Section5.jsx
@@ -0,0 +1,14 @@
+import './section5.scss' 
+
+export default function Section5(){
+    return(
+        <>
+            <section className="section5">
+                <div className="container">
+                    <h2>Travellers, meanwhile, are given transparency in the booking process</h2>
+                    <p>By providing reliable sustainability information on the booking platforms that consumers already know and trust, our coalition is helping everyone to learn and recognise what good looks like.</p>
+                </div>
+            </section>
+        </>
+    )
+}
