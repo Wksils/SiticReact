bb54b88a5ea3d57fff491b1e479b93fe9d382e51
YanaOw <y.ovchinnikova08@gmail.com>
2026-09-23 21:58:37 +0200

    connected section 8 with app

---
+++ react_project/src/components/Section8/Section8.jsx
@@ -0,0 +1,11 @@
+import './section8.scss'
+
+export default function Section8(){
+    return(
+        <>
+            <section className='section8'>
+
+            </section>
+        </>
+    )
+}
